77cb850f239823439c985438848c036034b83c4a
Erik Knudsen <31932266+erik1066@users.noreply.github.com>
2019-08-18 19:27:14 -0400

    Fixed several compiler warnings

--- src/Entities/ValueSet.cs
+++ src/Entities/ValueSet.cs
@@ -9,22 +9,22 @@ namespace Cdc.Vocabulary.Entities
         [Key]
         public Guid Id { get; set; }
 
-        public string Oid { get; set; }
+        public string? Oid { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [Required]
-        public string Code { get; set; }
+        public string? Code { get; set; }
 
-        public string Status { get; set; }
+        public string? Status { get; set; }
 
         public DateTimeOffset StatusDate { get; set; }
 
         [Required]
-        public string DefinitionText { get; set; }
+        public string? DefinitionText { get; set; }
 
-        public string ScopeNoteText { get; set; }
+        public string? ScopeNoteText { get; set; }
 
         public Guid AssigningAuthorityId { get; set; }
 
