5a1caf722ef502af9a7b15a3c567668db213b9ba
Erik Knudsen <31932266+erik1066@users.noreply.github.com>
2019-08-26 18:30:56 -0400

    Added Xml code comments to value set concept pagination parameters

--- src/Models/ValueSetConceptPaginationParameters.cs
+++ src/Models/ValueSetConceptPaginationParameters.cs
@@ -9,13 +9,22 @@ namespace Cdc.Vocabulary.WebApi.Models
     /// </summary>
     public class ValueSetConceptPaginationParameters : ValueSetVersionPaginationParameters
     {
+        /// <summary>
+        /// The value set version ID to filter on.
+        /// </summary>
         [FromQuery(Name = "valuesetversionid")]
         public Guid? ValueSetVersionId { get; set; }
 
+        /// <summary>
+        /// The value set version number to filter on. Using the special value 'latest' will automatically filter on the latest version number.
+        /// </summary>
         [FromQuery(Name = "valuesetversionnumber")]
         [RegularExpression("^([0-9]*|latest)$")]
         public string? ValueSetVersionNumber { get; set; }
 
+        /// <summary>
+        /// The concept code to filter on.
+        /// </summary>
         [FromQuery(Name = "conceptcode")]
         public string? ConceptCode { get; set; }
     }
