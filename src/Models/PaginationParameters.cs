14f99646e7fa8d9541c462bfd22bf2b8e7648a9b
Erik Knudsen <31932266+erik1066@users.noreply.github.com>
2019-08-26 18:20:00 -0400

    Added Xml code comments to pagination view model objects

--- src/Models/PaginationParameters.cs
+++ src/Models/PaginationParameters.cs
@@ -9,17 +9,29 @@ namespace Cdc.Vocabulary.WebApi.Models
     /// </summary>
     public class PaginationParameters
     {
+        /// <summary>
+        /// The page number to return
+        /// </summary>
         [Range(1, Int32.MaxValue)]
         [FromQuery(Name = "pageNumber")]
         public int PageNumber { get; set; } = 1;
 
+        /// <summary>
+        /// The maximum size of the result set that is returned
+        /// </summary>
         [Range(0, 100)]
         [FromQuery(Name = "pageSize")]
         public int PageSize { get; set; } = 10;
 
+        /// <summary>
+        /// A free-text search string that will be used for querying the resources
+        /// </summary>
         [FromQuery(Name = "searchQuery")]
         public string SearchQuery { get; set; } = string.Empty;
 
+        /// <summary>
+        /// Whether the search should use a 'contains' or 'startsWith' matching algorithm
+        /// </summary>
         [FromQuery(Name = "searchType")]
         public MatchType SearchType { get; set; } = MatchType.Contains;
     }
--- src/Models/ValueSetVersionPaginationParameters.cs
+++ src/Models/ValueSetVersionPaginationParameters.cs
@@ -9,9 +9,15 @@ namespace Cdc.Vocabulary.WebApi.Models
     /// </summary>
     public class ValueSetVersionPaginationParameters : PaginationParameters
     {
+        /// <summary>
+        /// The OID of the value set to filter on. Ex: 2.16.840.1.114222.4.11.888
+        /// </summary>
         [FromQuery(Name = "valuesetoid")]
         public string ValueSetOid { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The Code of the value set to filter on. Ex: PHVS_YesNoUnknown_CDC
+        /// </summary>
         [FromQuery(Name = "valuesetcode")]
         public string ValueSetCode { get; set; } = string.Empty;
     }
